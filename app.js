352798bfddd5ed1a6b8d821ae34daa9e179ba475
Isaiah Leiva <isaiah.leiva@gmail.com>
2022-02-08 16:28:10 -0700

    commit

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ const packages = [
     { heavy: false, priority: true, fragile: true, to: 'Mark', trackingNumber: '1325sdk', Missing: false },
     { heavy: true, priority: false, fragile: true, to: 'Mick', trackingNumber: 'jffd147', Missing: false },
     { heavy: false, priority: false, fragile: false, to: 'Jake', trackingNumber: 'acdc145', Missing: false },
-    { heavy: true, priority: true, fragile: true, to: 'Brittany', trackingNumber: 'hy16jk', Missing: false },
+    { heavy: true, priority: true, fragile: true, to: 'Brittany', trackingNumber: 'hya16jk', Missing: false },
     { heavy: false, priority: true, fragile: false, to: 'Zach', trackingNumber: '8081baz', Missing: false },
     { heavy: true, priority: false, fragile: true, to: 'Jeremy', trackingNumber: 'suz2367', Missing: false }]
+
 
 function filterHeavy() {
     let filteredArray = packages.filter(package => package.heavy == false)
     console.log(filteredArray)
+    draw()
 }
+
 
 function filterPriority() {
     let filteredArray = packages.filter(package => package.priority == false)
     console.log(filteredArray)
+    draw()
 }
+
 
 function filterFragile() {
     let filteredArray = packages.filter(package => package.fragile == false)
     console.log(filteredArray)
+    draw()
 }
+
 
 // Note to self. Look up spread operators
 
